73b872a806d5ecafa32b94b695a25a68aa2a0440
Oliver Rojas <olitroski@gmail.com>
2019-11-28 18:54:34 -0300

    mantencion

---
+++ shinny test/2.firstApp.r
@@ -0,0 +1,49 @@
+library(shiny)
+
+# User interface
+ui <- fluidPage(
+    # El input
+    selectInput("dataset", label = "Dataset", choices = ls("package:datasets")),
+    
+    # El output
+    verbatimTextOutput("summary"),
+    tableOutput("table")
+)
+
+# Server 
+server <- function(input, output, session) {
+    # El output para verbatimTextOutput("summary")
+    output$summary <- renderPrint({
+        dataset <- get(input$dataset, "package:datasets")
+        summary(dataset)
+    })
+    
+    # Para el <tableOutput("table")>
+    output$table <- renderTable({
+        dataset <- get(input$dataset, "package:datasets")
+        dataset
+    })
+}
+
+# Hacer funcionar
+shinyApp(ui, server)
+
+
+# -------------------------------------------------------------------------- #
+# El código anterior tiene codigo duplicado, para evitar eso se hace 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- shinny test/script.shiny.r
+++
@@ -1,6 +0,0 @@
-install.packages("shiny")
-
-library(shiny)
-setwd("D:/OneDrive/INTA/Actigrafia/shinny test")
-runApp()
-
